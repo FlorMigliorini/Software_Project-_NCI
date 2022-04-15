f6548255c18128009156ea2ae117b775f7d85c4d
FlorMigliorini <flor.migliorini@gmail.com>
2022-04-15 12:30:00 -0300

    Payment with pre-configured Stripe checkout

--- app/src/main/java/florence/migliorini/crossingborder/CheckoutActivity.java
+++ app/src/main/java/florence/migliorini/crossingborder/CheckoutActivity.java
@@ -1,5 +1,6 @@
 package florence.migliorini.crossingborder;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,25 +20,24 @@ import florence.migliorini.model.StripeKeyDTO;
 import florence.migliorini.payment.ConfigStripe;
 
 
-public class CheckoutActivity extends AppCompatActivity {
+public class CheckoutActivity{
     private static final String TAG = "CheckoutActivity";
     private Button payButton;
     PaymentSheet paymentSheet;
+    private Context context;
     String paymentIntentClientSecret;
     PaymentSheet.CustomerConfiguration customerConfig;
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    /*public CheckoutActivity(PaymentActivity instance, Context context){
         //setContentView(R.layout.activity_checkout);
         //payButton = findViewById(R.id.pay_button);
         //payButton.setOnClickListener(this::onPayClicked);
         //payButton.setEnabled(false);
-        paymentSheet = new PaymentSheet(this, this::onPaymentSheetResult);
+        this.context = context;
+        paymentSheet = new PaymentSheet(instance, this::onPaymentSheetResult);
 
         fetchPaymentIntent();
-    }
-    private void showAlert(String title, @Nullable String message) {
+    }*/
+    /*private void showAlert(String title, @Nullable String message) {
         runOnUiThread(() -> {
             AlertDialog dialog = new AlertDialog.Builder(this)
                     .setTitle(title)
@@ -46,54 +46,13 @@ public class CheckoutActivity extends AppCompatActivity {
                     .create();
             dialog.show();
         });
-    }
+    }*/
 
-    private void showToast(String message) {
+    /*private void showToast(String message) {
         runOnUiThread(() -> Toast.makeText(this, message, Toast.LENGTH_LONG).show());
-    }
+    }*/
 
-    private void fetchPaymentIntent() {
-        final String shoppingCartContent = "{\"items\": [ {\"id\":\"xl-tshirt\"}]}";
-        Gson gson = new Gson();
-        ConfigStripe config = new ConfigStripe();
-        StripeKeyDTO strpe = gson.fromJson(config.getKey(),StripeKeyDTO.class);
-        paymentIntentClientSecret = strpe.getPaymentIntentClientSecret();
 
-        customerConfig = new PaymentSheet.CustomerConfiguration(
-                strpe.getCustomer(),
-                strpe.getEphemeralKey()
-        );
-        PaymentConfiguration.init(getApplicationContext(),strpe.getPublishableKey());
-        PaymentSheet.Configuration configuration = new PaymentSheet.Configuration("Example, Inc.");
-
-        paymentSheet.presentWithPaymentIntent(paymentIntentClientSecret, configuration);
-        //runOnUiThread(() -> payButton.setEnabled(true));
-    }
-
-    void onPaymentSheetResult(final PaymentSheetResult paymentSheetResult) {
-        if (paymentSheetResult instanceof PaymentSheetResult.Canceled) {
-            Log.d("Stripe","Canceled");
-        } else if (paymentSheetResult instanceof PaymentSheetResult.Failed) {
-            Log.e("Stripe", "Got error: ", ((PaymentSheetResult.Failed) paymentSheetResult).getError());
-        } else if (paymentSheetResult instanceof PaymentSheetResult.Completed) {
-            // Display for example, an order confirmation screen
-            Log.d("Stripe","Completed");
-        }
-    }
-    private void presentPaymentSheet() {
-        final PaymentSheet.Configuration configuration = new PaymentSheet.Configuration.Builder("Example, Inc.")
-                .customer(customerConfig)
-                // Set `allowsDelayedPaymentMethods` to true if your business can handle payment methods
-                // that complete payment after a delay, like SEPA Debit and Sofort.
-                .allowsDelayedPaymentMethods(true)
-                .build();
-        //TEM UM ERRO AQUI
-        String paymentClientSecret = "sk_test_51KoodkK4jThQYIzhMivdqlsLJGCgcGDSpUoDPIh3bZTMdIWK8t30S1wVyrBMom78wWLwlAqVumgB6Li56HykEveD00cfvfIQA5";
-        paymentSheet.presentWithPaymentIntent(
-                paymentClientSecret,
-                configuration
-        );
-    }
     private void onPayClicked(View view) {
         /*PaymentSheet.Configuration configuration = new PaymentSheet.Configuration("Example, Inc.");
 
--- app/src/main/java/florence/migliorini/crossingborder/PaymentActivity.java
+++ app/src/main/java/florence/migliorini/crossingborder/PaymentActivity.java
@@ -4,21 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class PaymentActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.stripe.android.PaymentConfiguration;
+import com.stripe.android.paymentsheet.PaymentSheet;
+import com.stripe.android.paymentsheet.PaymentSheetResult;
 
-    private Button btnStripe,btnGooglePlayPay;
-    private TextView titleTicket,locationName,locationTime,destinationName,destinationTime,TimeTicket,numberPersons;
+import florence.migliorini.model.StripeKeyDTO;
+import florence.migliorini.payment.ConfigStripe;
+
+public class PaymentActivity extends AppCompatActivity {
+    PaymentSheet paymentSheet;
+    String paymentIntentClientSecret;
+    PaymentSheet.CustomerConfiguration customerConfig;
+
+    private Button btnStripe, btnGooglePlayPay;
+    private TextView titleTicket, locationName, locationTime, destinationName, destinationTime, TimeTicket, numberPersons;
     private ImageView imgTypeTransport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment);
+        paymentSheet = new PaymentSheet(this, this::onPaymentSheetResult);
         imgTypeTransport = findViewById(R.id.iconTypeTransport);
         titleTicket = findViewById(R.id.TitleTicket);
         locationName = findViewById(R.id.locationName);
@@ -37,18 +50,65 @@ public class PaymentActivity extends AppCompatActivity {
         /*Intent intent = new Intent(PaymentActivity.this,PlanActivity.class);
         startActivity(intent);
         finish();*/
+        PaymentActivity instance = this;
         btnStripe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(PaymentActivity.this, CheckoutActivity.class);
+                /*Intent intent = new Intent(PaymentActivity.this, CheckoutActivity.class);
                 startActivity(intent);
-                finish();
+                finish();*/
+                fetchPaymentIntent();
             }
         });
     }
+
     public void menuButton(View view) {
         Intent intent = new Intent(PaymentActivity.this, MenuActivity.class);
         startActivity(intent);
         finish();
     }
+
+    private void fetchPaymentIntent() {
+        final String shoppingCartContent = "{\"items\": [ {\"id\":\"xl-tshirt\"}]}";
+        Gson gson = new Gson();
+        ConfigStripe config = new ConfigStripe();
+        StripeKeyDTO strpe = gson.fromJson(config.getKey(),StripeKeyDTO.class);
+        paymentIntentClientSecret = strpe.getPaymentIntentClientSecret();
+
+        customerConfig = new PaymentSheet.CustomerConfiguration(
+                strpe.getCustomer(),
+                strpe.getEphemeralKey()
+        );
+        PaymentConfiguration.init(getApplicationContext(),strpe.getPublishableKey());
+        PaymentSheet.Configuration configuration = new PaymentSheet.Configuration("Example, Inc.");
+
+        paymentSheet.presentWithPaymentIntent(paymentIntentClientSecret, configuration);
+        //runOnUiThread(() -> payButton.setEnabled(true));
+    }
+
+    void onPaymentSheetResult(final PaymentSheetResult paymentSheetResult) {
+        if (paymentSheetResult instanceof PaymentSheetResult.Canceled) {
+            Log.d("Stripe","Canceled");
+        } else if (paymentSheetResult instanceof PaymentSheetResult.Failed) {
+            Log.e("Stripe", "Got error: ", ((PaymentSheetResult.Failed) paymentSheetResult).getError());
+        } else if (paymentSheetResult instanceof PaymentSheetResult.Completed) {
+            // Display for example, an order confirmation screen
+            Log.d("Stripe","Completed");
+        }
+    }
+    private void presentPaymentSheet() {
+        final PaymentSheet.Configuration configuration = new PaymentSheet.Configuration.Builder("Example, Inc.")
+                .customer(customerConfig)
+                // Set `allowsDelayedPaymentMethods` to true if your business can handle payment methods
+                // that complete payment after a delay, like SEPA Debit and Sofort.
+                .allowsDelayedPaymentMethods(true)
+                .build();
+        //TEM UM ERRO AQUI
+        String paymentClientSecret = "sk_test_51KoodkK4jThQYIzhMivdqlsLJGCgcGDSpUoDPIh3bZTMdIWK8t30S1wVyrBMom78wWLwlAqVumgB6Li56HykEveD00cfvfIQA5";
+        paymentSheet.presentWithPaymentIntent(
+                paymentClientSecret,
+                configuration
+        );
+    }
+
 }
