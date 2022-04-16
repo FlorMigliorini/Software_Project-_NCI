e78c2fbbf2eee312547b0098fe1f4d2c9c7bc31d
FlorMigliorini <flor.migliorini@gmail.com>
2022-04-16 18:32:33 -0300

    color alternation template favorites

--- app/src/main/java/florence/migliorini/crossingborder/FavoriteActivity.java
+++ app/src/main/java/florence/migliorini/crossingborder/FavoriteActivity.java
@@ -2,36 +2,28 @@ package florence.migliorini.crossingborder;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
-import static androidx.constraintlayout.widget.ConstraintProperties.PARENT_ID;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
-import florence.migliorini.db.DbFavorite;
-import florence.migliorini.db.DbHelper;
 import florence.migliorini.db.SQLiteMan;
-import florence.migliorini.model.Favorite;
 import florence.migliorini.model.TravelDTO;
 
 public class FavoriteActivity extends AppCompatActivity {
@@ -39,8 +31,12 @@ public class FavoriteActivity extends AppCompatActivity {
     private Button btnDelete,btnBack;
     private List<TravelDTO> listFavorites;
     private LinearLayout lnListFavorites;
+    private ConstraintLayout favoriteSelected = null;
+    private ConstraintLayout filterActive;
+    private ConstraintLayout iconFilterActive;
+    private TextView textViewActive;
 
-    @SuppressLint("ResourceAsColor")
+    @SuppressLint({"ResourceAsColor", "WrongViewCast"})
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +45,9 @@ public class FavoriteActivity extends AppCompatActivity {
         lnListFavorites = findViewById(R.id.lnListFavorites);
         btnDelete = findViewById(R.id.btnDelete);
         btnBack = findViewById(R.id.btnBack);
+        filterActive= findViewById(R.id.blockBtnAllTypes);
+        iconFilterActive = findViewById(R.id.btnAllTypes);
+        textViewActive = findViewById(R.id.txBtnAllTypes);
         /*DbHelper db = new DbHelper(FavoriteActivity.this);
         List<Favorite> favoriteList = db.getAllFavorites();
         Log.d("Test",favoriteList.toString());
@@ -109,10 +108,36 @@ public class FavoriteActivity extends AppCompatActivity {
                 ConstraintLayout ct = new ConstraintLayout(getApplicationContext());
                 ct.setBackgroundResource(R.drawable.shape_arredounded);
                 ct.setId(i++);
+                ct.setPadding(5,5,5,5);
                 ct.setMinWidth(lnListFavorites.getWidth());
                 ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(MATCH_PARENT,WRAP_CONTENT);
                 layoutParams.setMargins(0,0,0,50);
                 ct.setLayoutParams(layoutParams);
+
+                ct.setOnClickListener(new View.OnClickListener() {
+                    @SuppressLint("ResourceType")
+                    @Override
+                    public void onClick(View view) {
+                        if(favoriteSelected!=null){
+                            favoriteSelected.setBackgroundResource(R.drawable.shape_arredounded);
+                            ImageView iconTransportType = (ImageView) favoriteSelected.getViewById(favoriteSelected.getId()+1);
+                            iconTransportType.setImageResource(R.drawable.ic_baseline_directions_bus_24);
+                            TextView txLocation = (TextView) favoriteSelected.getViewById(favoriteSelected.getId()+2);
+                            txLocation.setTextColor(Color.rgb(56,56,56));
+                            TextView txDestine = (TextView) favoriteSelected.getViewById(favoriteSelected.getId()+4);
+                            txDestine.setTextColor(Color.rgb(56,56,56));
+                        }
+                        view.setBackgroundResource(R.drawable.shape_arredounded_blue);
+                        favoriteSelected = (ConstraintLayout) view;
+                        ImageView iconTransportType = (ImageView) favoriteSelected.getViewById(favoriteSelected.getId()+1);
+                        iconTransportType.setImageResource(R.drawable.ic_baseline_directions_bus_white_24);
+                        TextView txLocation = (TextView) favoriteSelected.getViewById(favoriteSelected.getId()+2);
+                        txLocation.setTextColor(Color.rgb(255,250,250));
+                        TextView txDestine = (TextView) favoriteSelected.getViewById(favoriteSelected.getId()+4);
+                        txDestine.setTextColor(Color.rgb(255,250,250));
+                    }
+                });
+
 
                 ImageView img = new ImageView(getApplicationContext());
                 img.setImageResource(R.drawable.ic_baseline_directions_bus_24);
@@ -124,6 +149,7 @@ public class FavoriteActivity extends AppCompatActivity {
                 layoutParams.horizontalBias = (float) 0.075;
                 //layoutParams.rightMargin = 8;
                 img.setLayoutParams(layoutParams);
+                //+1
                 img.setId(i++);
                 ct.addView(img);
 
@@ -135,14 +161,16 @@ public class FavoriteActivity extends AppCompatActivity {
                 layoutParams.baselineToTop = ct.getId();
                 layoutParams.verticalBias = (float) 0.508;
                 layoutParams.startToEnd = img.getId();
-                txLocation.setTextColor(R.color.black);
+                txLocation.setTextColor(Color.rgb(56,56,56));
                 txLocation.setTextSize(18);
                 txLocation.setLayoutParams(layoutParams);
+                //+2
                 txLocation.setId(i++);
                 ct.addView(txLocation);
 
                 ImageView imgArrow = new ImageView(getApplicationContext());
                 imgArrow.setImageResource(R.drawable.ic_baseline_arrow_right_alt_24);
+                //+3
                 imgArrow.setId(i++);
                 layoutParams = new ConstraintLayout.LayoutParams(100,100);
                 layoutParams.leftMargin = 100;
@@ -162,16 +190,100 @@ public class FavoriteActivity extends AppCompatActivity {
                 layoutParams.leftMargin = 80;
                 //layoutParams.startToStart = ct.getId();
                 txDestination.setLayoutParams(layoutParams);
+                //+4
                 txDestination.setId(i++);
-                txDestination.setTextColor(R.color.black);
+                txDestination.setTextColor(Color.rgb(56,56,56));
                 txDestination.setTextSize(18);
                 ct.addView(txDestination);
+
+                TextView txId = new TextView(getApplicationContext());
+                txId.setText(String.valueOf(tv.getId()));
+                //+5
+                txId.setId(i++);
+                txId.setVisibility(View.INVISIBLE);
+                //txId.setLayoutParams(layoutParams);
+                ct.addView(txId);
+
+                TextView txTypeTransport = new TextView(getApplicationContext());
+                txTypeTransport.setText(String.valueOf(tv.getId()));
+                //+6
+                txTypeTransport.setId(i++);
+                txTypeTransport.setVisibility(View.INVISIBLE);
+                //txId.setLayoutParams(layoutParams);
+                ct.addView(txTypeTransport);
                 lnListFavorites.addView(ct);
             }
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
     }
 
+    public void filterAll(View view){
+        if(view.getId()!=filterActive.getId()){
+            clearLastFilter();
+            view.setBackgroundResource(R.drawable.shape_arredounded_blue);
+            ConstraintLayout icon = findViewById(R.id.btnAllTypes);
+            TextView txBtn = findViewById(R.id.txBtnAllTypes);
+            icon.setBackgroundResource(R.drawable.ic_baseline_apps_white_24);
+            txBtn.setTextColor(Color.rgb(255,250,250));
+            filterActive = (ConstraintLayout) view;
+            textViewActive = txBtn;
+            iconFilterActive = icon;
 
+        }
+    }
+    public void filterBus(View view){
+        if(view.getId()!=filterActive.getId()){
+            clearLastFilter();
+            view.setBackgroundResource(R.drawable.shape_arredounded_blue);
+            ConstraintLayout icon = findViewById(R.id.btnBusType);
+            TextView txBtn = findViewById(R.id.txBtnBusType);
+            icon.setBackgroundResource(R.drawable.ic_baseline_directions_bus_white_24);
+            txBtn.setTextColor(Color.rgb(255,250,250));
+            filterActive = (ConstraintLayout) view;
+            textViewActive = txBtn;
+            iconFilterActive = icon;
+        }
+    }
+    public void filterTrain(View view){
+        if(view.getId()!=filterActive.getId()){
+            clearLastFilter();
+            view.setBackgroundResource(R.drawable.shape_arredounded_blue);
+            ConstraintLayout icon = findViewById(R.id.btnTrainType);
+            TextView txBtn = findViewById(R.id.txBtnTrainType);
+            icon.setBackgroundResource(R.drawable.ic_baseline_train_white_24);
+            txBtn.setTextColor(Color.rgb(255,250,250));
+            filterActive = (ConstraintLayout) view;
+            textViewActive = txBtn;
+            iconFilterActive = icon;
+        }
+    }
+    public void filterLuas(View view){
+        if(view.getId()!=filterActive.getId()){
+            clearLastFilter();
+            view.setBackgroundResource(R.drawable.shape_arredounded_blue);
+            ConstraintLayout icon = findViewById(R.id.btnLuasType);
+            TextView txBtn = findViewById(R.id.txBtnLuasType);
+            icon.setBackgroundResource(R.drawable.ic_baseline_subway_white_24);
+            txBtn.setTextColor(Color.rgb(255,250,250));
+            filterActive = (ConstraintLayout) view;
+            textViewActive = txBtn;
+            iconFilterActive = icon;
+        }
+    }
+    @SuppressLint("ResourceType")
+    public void clearLastFilter(){
+        filterActive.setBackgroundResource(R.drawable.shape_arredounded);
+        if(iconFilterActive.getId()==2131361935){
+            iconFilterActive.setBackgroundResource(R.drawable.ic_baseline_apps_24);
+        }else if(iconFilterActive.getId()==2131361951){
+            iconFilterActive.setBackgroundResource(R.drawable.ic_baseline_train_24);
+        }else if(iconFilterActive.getId()==2131361937){
+            iconFilterActive.setBackgroundResource(R.drawable.ic_baseline_directions_bus_24);
+        }else if(iconFilterActive.getId()==2131361944){
+            iconFilterActive.setBackgroundResource(R.drawable.ic_baseline_subway_24);
+        }
+        textViewActive.setTextColor(Color.rgb(56,56,56));
+    }
 }
